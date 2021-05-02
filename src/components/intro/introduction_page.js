2620f6a3decf235d76a5cf2a69972c4c4ed232f6
CapArken20 <aizhe5757@gmail.com>
2021-05-02 15:29:23 -0400

    Finished the intro page

---
+++ src/components/intro/introduction_page.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class TitlePage extends Component {
+    render() {
+        return (
+            <div>
+                <p>Welcome to the Life and Times. This is a CYOA (Choose Your Own Adventure) "game". This game was made by me for a capstone project that I needed to do. I also am doing this for a Aaronic Priesthood Theme Challenge. That is what this should be. This is the start of my first draft and we will see how it goes.</p>
+                {/* Link to next page */}
+            </div>
+        )
+    }
+}
--- src/components/intro/title_page.js
+++ src/components/intro/title_page.js
@@ -5,6 +5,10 @@ export default class TitlePage extends Component {
         return (
             <div>
                 <h1>The Life and Times</h1>
+                <h3>By CapArken20</h3>
+
+                <p>Begin your journey</p>
+                {/* Link to next page */}
             </div>
         )
     }
